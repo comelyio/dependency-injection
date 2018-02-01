2244a128c3c8043e82c7086cdc78994e14f8af55
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-01 13:49:43 +0500

    Exceptions

---
+++ Exception/RepositoryException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of Comely package.
+ * https://github.com/comelyio/comely
+ *
+ * Copyright (c) 2016-2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/comely/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\IO\DependencyInjection\Exception;
+
+/**
+ * Class RepositoryException
+ * @package Comely\IO\DependencyInjection\Exception
+ */
+class RepositoryException extends DependencyInjectionException
+{
+}
